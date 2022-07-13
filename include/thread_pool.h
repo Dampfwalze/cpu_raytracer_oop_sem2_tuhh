04de0ed2c94aa93fd4f10a90efe8d3b325871e1e
Dampfwalze <46897578+Dampfwalze@users.noreply.github.com>
2022-07-13 15:12:49 +0200

    Remove: Waiting features of ThreadPool

--- include/thread_pool.h
+++ include/thread_pool.h
@@ -40,13 +40,6 @@ namespace rt
         std::vector<std::thread> m_workers;
         EventStream<Event> m_stream;
 
-        uint64_t m_workingCount = 0;
-
-        std::function<void()> m_emptyCallback;
-
-        mutable std::mutex m_waitMutex;
-        mutable std::condition_variable m_waitCV;
-
     public:
         ThreadPool(size_t threadCount);
 
@@ -55,15 +48,9 @@ namespace rt
 
         ~ThreadPool();
 
-        inline bool isWorking() const { return m_workingCount > 0; }
-
-        inline void setEmptyCallback(const std::function<void()> &emptyCallback) { m_emptyCallback = emptyCallback; }
-
         inline bool isEmpty() const { return m_stream.isEmpty(); }
 
         inline void clear() { m_stream.clear(); }
-
-        void wait() const;
 
         ThreadPool &operator<<(const task_type &task);
         ThreadPool &operator<<(task_type &&task);
@@ -94,7 +81,6 @@ namespace rt
 
     template <typename _Task>
     ThreadPool<_Task>::ThreadPool(size_t threadCount)
-        : m_workingCount(0)
     {
         m_workers.reserve(threadCount);
         for (size_t i = 0; i < threadCount; i++)
@@ -115,47 +101,15 @@ namespace rt
     template <typename _Task>
     void ThreadPool<_Task>::run()
     {
-        bool notFirst = false;
-
         while (true)
         {
-            auto lk = m_stream.getLock();
-
-            if (notFirst)
-            {
-                m_workingCount--;
-                if (m_workingCount == 0 && m_stream.isEmpty())
-                {
-                    m_waitCV.notify_all();
-                    if (m_emptyCallback)
-                    {
-                        lk.unlock();
-                        m_emptyCallback();
-                        lk.lock();
-                    }
-                }
-            }
-            notFirst = true;
-
-            Event event = std::move(m_stream.get(lk));
+            Event event = std::move(m_stream.get());
 
             if (event.terminate)
                 break;
 
-            m_workingCount++;
-            lk.unlock();
-
             event.task();
         }
-    }
-
-    template <typename _Task>
-    void ThreadPool<_Task>::wait() const
-    {
-        if (m_stream.isEmpty())
-            return;
-        std::unique_lock lk(m_waitMutex);
-        m_waitCV.wait(lk);
     }
 
     template <typename _Task>
